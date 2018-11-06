ec7b6dc67eaaf74f95409bda4492b2b122157df1
Martincz Gao <martincz@mokeedev.com>
2018-11-07 03:44:01 +0800

    discovery: mokee-ify

--- mokee.mk
+++ mokee.mk
@@ -16,10 +16,10 @@
 
 $(call inherit-product, device/sony/discovery/full_discovery.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_discovery
+PRODUCT_NAME := mk_discovery
 PRODUCT_DEVICE := discovery
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := Xperia XA2 Ultra
